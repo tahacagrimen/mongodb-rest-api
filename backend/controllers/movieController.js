866b74700ba9f51f5d8a647e6cb76046a0e2f0c5
tahacagrimen <tahacagrimen2@gmail.com>
2022-09-02 14:57:24 +0300

    next stage adding mongodb

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,47 +1,49 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get movies
 // @route GET /api/movies
 // @access Private
-const getMovies = (req, res) => {
+const getMovies = asyncHandler((req, res) => {
   res.status(200).json({
     message: "Welcome to the movies API",
   });
-};
+});
 
 // @desc Get movie by id
 // @route GET /api/movies/:id
 // @access Private
-const getMovie = (req, res) => {
+const getMovie = asyncHandler((req, res) => {
   res.status(200).json({
     message: "movie with id " + req.params.id,
   });
-};
+});
 
 // @desc Set movie
 // @route POST /api/movies
 // @access Private
-const setMovie = (req, res) => {
+const setMovie = asyncHandler((req, res) => {
   res.status(200).json({
     message: "POST request to /api/movies",
   });
-};
+});
 
 // @desc Update movie
 // @route PUT /api/movies/:id
 // @access Private
-const updateMovie = (req, res) => {
+const updateMovie = asyncHandler((req, res) => {
   res.status(200).json({
     message: "Updated movie with id " + req.params.id,
   });
-};
+});
 
 // @desc Delete movie
 // @route DELETE /api/movies/:id
 // @access Private
-const deleteMovie = (req, res) => {
+const deleteMovie = asyncHandler((req, res) => {
   res.status(200).json({
     message: "Deleted movie with id " + req.params.id,
   });
-};
+});
 
 module.exports = {
   getMovies,
